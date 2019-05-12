f88ab9b4950de3d72e594f09c24d92ff021eb678
naddana92 <naddana2@gmail.com>
2019-05-12 18:19:55 +0900

    feat<Hint> refactoring

--- src/main/java/io/github/wotjd243/findbyhint/hint/domain/HintInventory.java
+++ src/main/java/io/github/wotjd243/findbyhint/hint/domain/HintInventory.java
@@ -1,10 +1,10 @@
 package io.github.wotjd243.findbyhint.hint.domain;
 
-public class HintEffect {
+public class HintInventory {
     private final Long treasureId;
     private BringTarget bringtargetId;
 
-    public HintEffect(final Long treasureId, BringTarget bringtargetId) {
+    public HintInventory(final Long treasureId, BringTarget bringtargetId) {
         this.treasureId = treasureId;
         this.bringtargetId = bringtargetId;
     }
