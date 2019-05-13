158296d77136cecfba6e124cd7991ce24f9bfda7
92kimyoungchan <naddana1@naver.com>
2019-05-13 18:12:51 +0900

    feat<Hint>

--- src/main/java/io/github/wotjd243/findbyhint/hint/domain/HintRepository.java
+++ src/main/java/io/github/wotjd243/findbyhint/hint/domain/HintRepository.java
@@ -1,8 +1,16 @@
 package io.github.wotjd243.findbyhint.hint.domain;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 
 // save , List , delete
-public interface HintRepository {
+public interface HintRepository extends JpaRepository<Hint,Long> {
     Optional<Hint> findById(final Long id);
+
+    Optional<HintInventory> findById2(final Long id);
+
+    List<Long> findHintInventory(@Param("treasureId") Long treasureId, @Param("ids") List<Long> ids);
 }
