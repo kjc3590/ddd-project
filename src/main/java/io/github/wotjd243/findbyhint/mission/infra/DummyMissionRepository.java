a09260c8b22f08d3a71597471b954c1ee5329a6e
ppiyo5 <ppiyo5@gmail.com>
2019-05-13 18:51:47 +0900

    no message

--- src/main/java/io/github/wotjd243/findbyhint/mission/infra/DummyMissionRepository.java
+++ src/main/java/io/github/wotjd243/findbyhint/mission/infra/DummyMissionRepository.java
@@ -1,18 +1,18 @@
-package io.github.wotjd243.findbyhint.mission.infra;
-
-import io.github.wotjd243.findbyhint.mission.domain.Mission;
-import io.github.wotjd243.findbyhint.mission.domain.MissionLevel;
-import io.github.wotjd243.findbyhint.mission.domain.MissionRepository;
-import org.springframework.stereotype.Repository;
-
-import java.io.IOException;
-import java.util.Optional;
-
-@Repository
-public class DummyMissionRepository implements MissionRepository {
-
-    @Override
-    public Optional<Mission> findById(Long missionId) {
-        return Optional.empty();
-    }
-}
+//package io.github.wotjd243.findbyhint.mission.infra;
+//
+//import io.github.wotjd243.findbyhint.mission.domain.Mission;
+//import io.github.wotjd243.findbyhint.mission.domain.MissionLevel;
+//import io.github.wotjd243.findbyhint.mission.domain.MissionRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.io.IOException;
+//import java.util.Optional;
+//
+//@Repository
+//public class DummyMissionRepository implements MissionRepository {
+//
+//    @Override
+//    public Optional<Mission> findById(Long missionId) {
+//        return Optional.empty();
+//    }
+//}
