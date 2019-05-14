294642d929ed5fa83fd4056a940adb5e41f55db6
92kimyoungchan <naddana1@naver.com>
2019-05-14 15:02:50 +0900

    fix mission entity

--- src/main/java/io/github/wotjd243/findbyhint/mission/domain/MissionInventory.java
+++ src/main/java/io/github/wotjd243/findbyhint/mission/domain/MissionInventory.java
@@ -10,8 +10,7 @@ public class MissionInventory {
 
     private Long missionId;
 
-    @ColumnDefault("N")
-    private String success;
+    private String success ="N";
 
     private MissionQnA missionQnA;
 
