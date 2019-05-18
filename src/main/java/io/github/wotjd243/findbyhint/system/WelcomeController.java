19b0a3396a1a304e3c39e71152b9dbcd7c696596
doyoung0205 <doyoung0205@naver.com>
2019-05-19 00:10:25 +0900

    feat system controller, refactor index, mapTest

---
+++ src/main/java/io/github/wotjd243/findbyhint/system/WelcomeController.java
@@ -0,0 +1,15 @@
+package io.github.wotjd243.findbyhint.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/index")
+    public String welcome() {
+        return "/index";
+    }
+
+}
